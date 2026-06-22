9e5b038fa5996c499ff255feb639a1105f48de5c
d0ofus0 <jainsamyak.2018@gmail.com>
2026-06-22 00:10:43 -0400

    Problem 44

---
+++ problem44.py
@@ -0,0 +1,14 @@
+pentagons = set()
+
+for i in range(1, 10_000):
+    pentagons.add(int(i * (3 * i - 1)/2))
+
+sols = set()
+
+for num1 in pentagons:
+    for num2 in pentagons:
+        if num1 + num2 in pentagons and abs(num1 - num2) in pentagons:
+            sols.add(abs(num1 - num2))
+
+print(min(sols))
+        
