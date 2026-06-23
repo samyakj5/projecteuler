6f00abc9c64427195a544e5ac26ef1dd597a4126
d0ofus0 <jainsamyak.2018@gmail.com>
2026-06-23 10:03:18 -0400

    Problem 357, WIP

---
+++ problem357.py
@@ -0,0 +1,9 @@
+# we know that odd numbers cannot be prime-generating, 
+# because for any odd number 2k + 1, 1 + (2k + 1)/1 = 2k + 2
+# which is divisible by 2.
+
+# any 2+ digit number ending with 4 or 6 also cannot be prime-generating:
+# if we have ...6, then 2 + ...6/2 = 2 + ...3 = ...5, divisible by 5
+# if we have ...4, then 1 + ...4/1 = ...5, divisible by 5
+
+# so we only have to consider numbers ending with 0, 2, or 8.
