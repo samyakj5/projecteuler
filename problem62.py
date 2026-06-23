290034c571a4625d2a384806d59f515741374955
d0ofus0 <jainsamyak.2018@gmail.com>
2026-06-23 17:22:47 -0400

    Finished Problem 62

--- problem62.py
+++ problem62.py
@@ -1,44 +1,61 @@
 import math
 
-def permutations(elements):
-    if len(elements) == 1:
-        return [elements]
+# def permutations(elements):
+#     if len(elements) == 1:
+#         return [elements]
     
-    result = []
-    for i, current in enumerate(elements):
-        remaining_elements = elements[:i] + elements[i + 1:]
+#     result = []
+#     for i, current in enumerate(elements):
+#         remaining_elements = elements[:i] + elements[i + 1:]
 
-        for p in permutations(remaining_elements):
-            result.append([current] + p)
+#         for p in permutations(remaining_elements):
+#             result.append([current] + p)
 
-    return result
+#     return result
 
-cubes = set()
+cubes = {}
 
 for i in range(10_000):
 
     count = 1
-    cube_set = {i**3}
 
-    for p in permutations(list(str(i**3))):
+    perm = tuple(sorted(str(i**3)))
 
-        p_num = "".join(p)
+    c, n = 0, i
 
-        if p_num[0] == "0":
-            continue
+    if cubes.get(perm):
+        c, n = cubes[perm]
 
-        p_num = int(p_num)
+    cubes[perm] = (c + 1, n)
 
-        if p_num > i**3:
-            continue
-        
-        if p_num in cubes and p_num not in cube_set:
-            count += 1
-            cube_set.add(int(p_num))
-    
-    if count == 5:
-        print(cube_set)
-        print(min(cube_set))
+    if cubes[perm][0] == 5:
+        print(cubes[perm][1]**3)
         break
 
-    cubes.add(i**3)
+
+
+
+
+    # for p in permutations(list(str(i**3))):
+
+    #     p_num = "".join(p)
+
+    #     if p_num[0] == "0":
+    #         continue
+
+    #     p_num = int(p_num)
+
+    #     if p_num > i**3:
+    #         continue
+        
+    #     if p_num in cubes and p_num not in cube_set:
+    #         count += 1
+    #         cube_set.add(int(p_num))
+    #         print(i, count)
+    
+    # if count == 5:
+    #     print(cube_set)
+    #     print(min(cube_set))
+    #     break
+
+    # cubes.add(i**3)
