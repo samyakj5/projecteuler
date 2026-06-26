9855998b57b086989744c16252f523cacb3f1a01
d0ofus0 <jainsamyak.2018@gmail.com>
2026-06-26 10:13:48 -0400

    Problem 53

---
+++ problem53.py
@@ -0,0 +1,10 @@
+import math
+
+count = 0
+
+for n in range(1, 101):
+    for r in range(1, n + 1):
+        if math.factorial(n) / (math.factorial(r) * math.factorial(n - r)) > 1_000_000:
+            count += 1
+
+print(count)
