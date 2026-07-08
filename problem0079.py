0a7bab55a5c6312fc66404ff969a259363f4ab6e
d0ofus0 <jainsamyak.2018@gmail.com>
2026-07-08 11:01:58 -0400

    Problem 79

---
+++ problem0079.py
@@ -0,0 +1,9 @@
+with open("problem0079_data.txt", "r", encoding="utf-8") as file:
+    lines = file.read()
+
+lines = lines.split("\n")
+
+nums = set(lines)
+
+# print(sorted(nums))
+# I solved this one by just looking at the numbers: 73162890
